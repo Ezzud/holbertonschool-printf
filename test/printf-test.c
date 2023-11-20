68f317b8cc4b12cdb158272bf8adb4448ee642d7
Ezzud <contact@ezzud.fr>
2023-11-20 04:37:14 -0800

    Code optimization & heading + Put all util functions in a utils.c file + Added utils function in header + Optimized and commented main file

--- test/printf-test.c
+++ test/printf-test.c
@@ -1,46 +1,5 @@
-#include <unistd.h>
 #include <stdarg.h>
 #include "printf.h"
-/**
- * get_int_digits - Get the number of digits of an int
- * @v: Integer to check
- * Return: Number of digits
- */
-int get_int_digits(int v) {
-	int count = 0;
-	int temp = v;
-	if(temp < 0) count++;
-	do {
-	    temp /= 10;
-	    ++count;
-  	} while (temp != 0);
-  	return (count);
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * print_number - Print a number in the stdout
- * @i: The number to print
- */
-void print_number(int i) {
-	if(i < 0)
-	{
-		_putchar('-');
-		i = -i;
-	}
-    if (i/10)
-        print_number(i/10);
-    _putchar(i%10 + '0');
-}
 /*
  * _printf - Format a text and print it
  * @format: Original string to be formatted
@@ -58,22 +17,32 @@ int _printf(const char* format, ...)
 	{
 		currentChar = *format;
 		format++;
+
+		/*
+		 * Check if char is a starts of formatting parameter
+		 */
 		if ( currentChar == '%' )
 		{
 			char nextChar = *format++;
+			/*
+			 * Check if parameter is a int
+			 */
 			if (nextChar == 'd')
 			{
 				int int_value = (int) va_arg( arg_l, int );
-				print_number(int_value);
-				printedChars+=get_int_digits(int_value);
-			} else if (nextChar == 's')
+				printedChars+=print_number(int_value);
+			}
+			/*
+			 *  Check if parameter is a string 
+			 */
+			else if (nextChar == 's')
 			{
 				const char *str_value = (const char *) va_arg( arg_l, const char *);
-				while( *str_value != '\0' ) {
-					_putchar(*str_value);
-					str_value++;
-					printedChars++;
-				}
+				printedChars += print_string(str_value);
+			} else {
+				/*
+				 * Error detected (no recognized parameter)
+				 */
 			}
 		} else {
 			_putchar(currentChar);
--- test/printf.h
+++ test/printf.h
@@ -1,4 +1,8 @@
 #ifndef _PRINTF_
 #define _PRINTF_
 int _printf(const char* format, ...);
+int get_int_digits(int v);
+int _putchar(char c);
+int print_number(int i);
+int print_string(const char* str_value);
 #endif
