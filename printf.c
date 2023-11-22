fe21c2a96aab3bcea3454f0857d47a93d05f591d
Ezzud <contact@ezzud.fr>
2023-11-22 07:45:45 -0800

    Update root files to correspond to new updates

--- printf.c
+++ printf.c
@@ -1,4 +1,6 @@
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
 #include "printf.h"
 /**
  * _printf - Format a text and print it
@@ -9,8 +11,10 @@
 int _printf(const char *format, ...)
 {
 	va_list arg_l;
-	int printedChars = 0, int_value;
+	int printedChars = 0, int_value, uint_value;
 	char currentChar, chr_value;
+	const char *str_value;
+	char *binary_value;
 
 	va_start(arg_l, format);
 	while (*format != '\0')
@@ -22,30 +26,46 @@ int _printf(const char *format, ...)
 		{
 			char nextChar = *format++;
 
-			if (nextChar == 'd')
+			switch(nextChar)
 			{
-				int_value = (int) va_arg(arg_l, int);
-				printedChars += print_number(int_value);
-			} else if (nextChar == 'i')
-			{
-				int_value = (int) va_arg(arg_l, int);
-				printedChars += print_number(int_value);
-			} else if (nextChar == 'c')
-			{
-				chr_value = (int) va_arg(arg_l, int);
-				printedChars += _putchar(chr_value);
-			} else if (nextChar == 's')
-			{
-				const char *str_value = (const char *) va_arg(arg_l, const char *);
-
-				printedChars += print_string(str_value);
+				case 'b':
+					uint_value = va_arg(arg_l, unsigned int);
+					binary_value = i_to_binary(uint_value);
+					printedChars += print_string(binary_value);
+					break;
+				case 'd':
+					int_value = va_arg(arg_l, int);
+					printedChars += print_number(int_value);
+					break;
+				case 'i':
+					int_value = va_arg(arg_l, int);
+					printedChars += print_number(int_value);
+					break;
+				case 'u':
+					uint_value = va_arg(arg_l, unsigned int);
+					printedChars += print_unsigned_number(uint_value);
+					break;
+				case 'c':
+					chr_value = (int) va_arg(arg_l, int);
+					printedChars += _putchar(chr_value);
+					break;
+				case 's':
+					str_value = (const char *) va_arg(arg_l, const char *);
+					printedChars += print_string(str_value);
+					break;
+				default:
+					_putchar(currentChar);
+					_putchar(nextChar);
+					printedChars+=2;
+					break;
 			}
 		} else
 		{
 			_putchar(currentChar);
 			printedChars++;
 		}
-		va_end(arg_l);
+
 	}
+	va_end(arg_l);
 	return (printedChars);
 }
--- utils.c
+++ utils.c
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "printf.h"
 /**
  * get_int_digits - Get the number of digits of an int
@@ -43,6 +46,43 @@ int print_number(int i)
 		_putchar('-');
 		i = -i;
 	}
+	if (i / 10)
+		print_number(i / 10);
+	_putchar(i % 10 + '0');
+	return (dig_count);
+}
+/**
+ * i_to_binary - Convert an unsigned int to binary
+ * @i: The number to convert
+ * Return: The binary number as a int
+ */
+char *i_to_binary(unsigned int x)
+{
+	int binaryNum[32];
+	char *binaryText = malloc(sizeof(char) * 32);
+   	int i=0;
+   	int j;
+
+	if (x == 0)
+		return (0);
+
+	for (;x > 0;)
+	{
+    	binaryNum[i++] = x % 2;
+		x /= 2;
+   	}
+   	for (j = i-1; j >= 0; j--)
+		binaryText[i-(j+1)] = binaryNum[j] + '0';
+   	return (binaryText);
+}
+/**
+ * print_unsigned_number - Print a number in the stdout
+ * @i: The number to print
+ * Return: Number of chars printed
+ */
+int print_unsigned_number(unsigned int i)
+{
+	int dig_count = get_int_digits(i);
 	if (i / 10)
 		print_number(i / 10);
 	_putchar(i % 10 + '0');
