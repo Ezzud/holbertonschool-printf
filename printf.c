35be7bf961a448aecc2a0b3ff11dba68a18fa609
Ezzud <contact@ezzud.fr>
2023-11-23 02:46:34 -0800

    Code optimization

--- printf.c
+++ printf.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "main.h"
 /**
  * _printf - Format a text and print it
@@ -11,10 +12,8 @@
 int _printf(const char *format, ...)
 {
 	va_list arg_l;
-	int printedChars = 0, int_value, uint_value;
-	char currentChar, chr_value;
-	const char *str_value;
-	char *binary_value, *hexa_value;
+	int printedChars = 0;
+	char currentChar;
 
 	va_start(arg_l, format);
 	while (*format != '\0')
@@ -26,59 +25,37 @@ int _printf(const char *format, ...)
 		{
 			char nextChar = *format++;
 
-			switch(nextChar)
+			switch (nextChar)
 			{
 				case '%':
-					_putchar('%');
-					printedChars++;
+					printedChars += _putchar(nextChar);
 					break;
 				case 'x':
-					uint_value = va_arg(arg_l, unsigned int);
-					hexa_value = i_to_hex(uint_value, 0);
-					printedChars += print_string(hexa_value);
-					break;
 				case 'X':
-					uint_value = va_arg(arg_l, unsigned int);
-					hexa_value = i_to_hex(uint_value, 1);
-					printedChars += print_string(hexa_value);
+					printedChars += print_string(i_to_hex(va_arg(arg_l, unsigned int), isupper(nextChar) ? 1 : 0));
 					break;
 				case 'b':
-					uint_value = va_arg(arg_l, unsigned int);
-					binary_value = i_to_binary(uint_value);
-					printedChars += print_string(binary_value);
+					printedChars += print_string(i_to_binary(va_arg(arg_l, unsigned int)));
 					break;
 				case 'd':
-					int_value = va_arg(arg_l, int);
-					printedChars += print_number(int_value);
-					break;
 				case 'i':
-					int_value = va_arg(arg_l, int);
-					printedChars += print_number(int_value);
+					printedChars += print_number(va_arg(arg_l, int));
 					break;
 				case 'u':
-					uint_value = va_arg(arg_l, unsigned int);
-					printedChars += print_unsigned_number(uint_value);
+					printedChars += print_unsigned_number(va_arg(arg_l, unsigned int));
 					break;
 				case 'c':
-					chr_value = (int) va_arg(arg_l, int);
-					printedChars += _putchar(chr_value);
+					printedChars += _putchar((int) va_arg(arg_l, int));
 					break;
 				case 's':
-					str_value = (const char *) va_arg(arg_l, const char *);
-					printedChars += print_string(str_value);
+					printedChars += print_string((const char *) va_arg(arg_l, const char *));
 					break;
 				default:
-					_putchar(currentChar);
-					_putchar(nextChar);
-					printedChars+=2;
+					printedChars += (_putchar(currentChar) + _putchar(nextChar));
 					break;
 			}
 		} else
-		{
-			_putchar(currentChar);
-			printedChars++;
-		}
-
+			printedChars += _putchar(currentChar);
 	}
 	va_end(arg_l);
 	return (printedChars);
